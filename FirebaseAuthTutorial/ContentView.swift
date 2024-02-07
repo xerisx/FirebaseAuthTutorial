ec8d1e342bc661c4fca754ab3bf6c72bc1b375a7
xerisx <k.61507991@gmail.com>
2024-02-07 14:46:38 +0900

    Initial Commit

---
+++ FirebaseAuthTutorial/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FirebaseAuthTutorial
+//
+//  Created by Shohei Hirota on 2024/02/07.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FirebaseAuthTutorial/FirebaseAuthTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirebaseAuthTutorialApp.swift
+//  FirebaseAuthTutorial
+//
+//  Created by Shohei Hirota on 2024/02/07.
+//
+
+import SwiftUI
+
+@main
+struct FirebaseAuthTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
